bfff45167b6db4c1d2793fbd24b32838dd1a6eb5
Andrew Ferri <andrew.f@whitespowersports.com>
2025-09-12 09:26:27 +1200

    .

--- script.js
+++ script.js
@@ -94,12 +94,13 @@ const YTRS = {
     },
 
     initPlayers: function() {
-        if (this.ytVideoUrlInput.value.match(/v=/i) !== null && this.localVideoSelect.files.length > 0)
+        let id = this.youtubeId(this.ytVideoUrlInput.value);
+        if (id !== '' && this.localVideoSelect.files.length > 0)
         {
             let self = this;
 
             this.ytPlayer = new YT.Player('yt-player', {
-                videoId: this.ytVideoUrlInput.value.split('v=').pop().replace(/^([A-Za-z0-9]+).*$/i, '$1'),
+                videoId: id,
                 width: '640',
                 height: '390',
                 playerVars: {playsinline:1},
@@ -145,6 +146,19 @@ const YTRS = {
         {
             this.ytPlayerContainer.removeAttribute('style');
         }
+    },
+
+    youtubeId: function(url) {
+        let id = '';
+        url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+        if(url[2] !== undefined)
+        {
+            id = url[2].split(/[^0-9a-z_\-]/i)[0];
+        } else
+        {
+            id = url;
+        }
+        return id;
     },
 };
 
