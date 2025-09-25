28d457dc2a7fb4ce4feb285fec1a01a85159920a
Andrew Ferri <andrew.f@whitespowersports.com>
2025-09-25 15:36:07 +1200

    Use cash.js library

--- ytrs.js
+++ ytrs.js
@@ -20,33 +20,34 @@ const YTRS = {
     localVideoValid: false,
     init: function()
     {
-        this.root = document.getElementById('root')
-        this.step = document.getElementById('step')
+        this.root = $('#root')
+        this.step = $('#step')
         this.step1()
 
-        let faq = document.getElementById('faq')
-        document.getElementById('faq-open').addEventListener('click', function(e){
+        let faq = $('#faq')
+        $('#faq-open').on('click', function(e){
             e.preventDefault()
-            faq.classList.add('show')
+            faq.addClass('show')
         })
-        document.getElementById('faq-close').addEventListener('click', function(e){
+        $('#faq-close').on('click', function(e){
             e.preventDefault()
-            faq.classList.remove('show')
+            faq.removeClass('show')
         })
     },
     step1: function() // YT URL
     {
         let self = this
-        self.root.className = 'step1'
+        self.root.attr('class', 'step1')
 
         let timeout = null
         let warning = null
-        let button = document.querySelectorAll('button.btn')[0]
-        let input = document.querySelectorAll('input.text')[0]
+        let button = $('button.btn')
+        let input = $('input.text')
 
-        input.addEventListener('input', function(e)
+        input.on('input', function(e)
         {
             self.YTVideoURL = e.target.value
+            console.log(e.target.value)
 
             if (warning !== null)
             {
@@ -59,22 +60,20 @@ const YTRS = {
             {
                 timeout = setTimeout(function()
                 {
-                    let note = document.createElement('p')
-                    note.innerText = 'Play the video until you reach the beginning of the reaction, then click "Next".'
-                    self.step.appendChild(note)
+                    let note = $('<p>Play the video until you reach the beginning of the reaction, then click "Next".</p>')
+                    self.step.append(note)
 
-                    let wrap = document.createElement('div')
-                    wrap.className = 'yt-wrapper'
-                    self.step.appendChild(wrap)
+                    let wrap = $('<div class="yt-wrapper" />')
+                    self.step.append(wrap)
 
-                    let div = document.createElement('div')
-                    div.setAttribute('id', 'yt-player')
-                    wrap.appendChild(div)
+                    let div = $('<div id="yt-player" />')
+                    wrap.append(div)
+                    console.log(div[0])
 
                     try {
                         div.innerHTML = ''
 
-                        let player = new YT.Player(div, {
+                        let player = new YT.Player(div[0], {
                             videoId: self.youtubeId(),
                             width: '640',
                             height: '390',
@@ -82,12 +81,12 @@ const YTRS = {
                             events: {
                                 onStateChange: function()
                                 {
-                                    button.removeAttribute('disabled')
+                                    button.removeAttr('disabled')
                                 },
                             }
                         })
 
-                        button.addEventListener('click', function()
+                        button.on('click', function()
                         {
                             self.videoOffset = player.getCurrentTime()
                             self.step2()
@@ -97,13 +96,11 @@ const YTRS = {
                     } catch(e) {
                         note.remove()
                         wrap.remove()
-                        button.setAttribute('disabled', 'disabled')
+                        button.attr('disabled', 'disabled')
                         self.YTVideoValid = false
 
-                        warning = document.createElement('div')
-                        warning.className = 'warning-message'
-                        warning.innerHTML = 'Invalid video URL'
-                        self.step.appendChild(warning)
+                        warning = $('<div class="warning-message">Invalid video URL</div>')
+                        self.step.append(warning)
                     }
                 }, 600)
             }
@@ -113,39 +110,32 @@ const YTRS = {
     {
         let self = this
         self._clear()
-        self.root.className = 'step2'
+        self.root.attr('class', 'step2')
 
-        let note = document.createElement('div')
-        note.className = 'block'
-        note.innerHTML = 'Select local video file'
+        let note = $('<div class="block">Select local video file</div>')
 
-        let button = document.createElement('button')
-        button.className = 'btn'
-        button.innerHTML = 'Next &rarr;'
-        button.setAttribute('disabled', 'disabled')
-        button.addEventListener('click', self.validate_step2.bind(self))
+        let button = $('<button class="btn" disabled>Next &rarr</button>')
+        button.on('click', self.validate_step2.bind(self))
 
-        let input = document.createElement('input')
-        input.setAttribute('type', 'file')
-        input.addEventListener('change', function(e)
+        let input = $('<input type="file" />')
+        input.on('change', function(e)
         {
             self.localVideoURL = URL.createObjectURL(e.target.files[0])
-            let el = document.getElementById('local-player')
-            if (el === null)
+            let el = $('#local-player')
+            if (el.length <= 0)
             {
-                el = document.createElement('div')
-                el.setAttribute('id', 'local-player')
-                self.step.appendChild(el)
+                el = $('<div id="local-player" />')
+                self.step.append(el)
             }
 
-            el.innerHTML = ''
+            el.html('')
             self.loadLocalPlayer(true)
 
             setTimeout(function()
             {
                 if (self.localVideoValid === true)
                 {
-                    button.removeAttribute('disabled')
+                    button.removeAttr('disabled')
                 } else
                 {
                     el.remove()
@@ -153,12 +143,11 @@ const YTRS = {
             }, 100)
         })
 
-        let div = document.createElement('div')
-        div.className = 'block inputs'
-        div.appendChild(input)
-        div.appendChild(button)
-        self.step.appendChild(note)
-        self.step.appendChild(div)
+        let div = $('<div class="block inputs" />')
+        div.append(input)
+        div.append(button)
+        self.step.append(note)
+        self.step.append(div)
     },
     validate_step2: function()
     {
@@ -177,55 +166,47 @@ const YTRS = {
     {
         let self = this
         self._clear()
-        self.root.className = 'step3'
+        self.root.attr('class', 'step3')
 
-        document.getElementById('faq-toggle').remove()
+        $('#faq-toggle').remove()
 
         if (localStorage.getItem('infoCleared') === null)
         {
-            let info = document.createElement('div')
-            info.className = 'info'
-            info.innerHTML = "<p>Use the right edge of the Youtube video to resize. Use the other edges to drag and reposition.<br/>When you're ready, click play on the Youtube video. The local video will sync with the Youtube video when you play/pause/seek.</p>"
-            self.step.appendChild(info)
+            let info = $('<div class="info"><p>Use the right edge of the Youtube video to resize. Use the other edges to drag and reposition.<br/>When you\'re ready, click play on the Youtube video. The local video will sync with the Youtube video when you play/pause/seek.</p></div>')
+            self.step.append(info)
 
-            let a = document.createElement('a')
-            a.setAttribute('href', '#')
-            a.innerHTML = '&times;'
-            a.addEventListener('click', function(e)
+            let a = $('<a href="#">&times;</a>')
+            a.on('click', function(e)
             {
                 e.preventDefault()
                 info.remove()
                 localStorage.setItem('infoCleared', '1')
             })
-            info.appendChild(a)
+            info.append(a)
         }
 
-        let div = document.createElement('div')
-        div.setAttribute('id', 'local-player')
-        self.step.appendChild(div)
+        let div = $('<div id="local-player" />')
+        self.step.append(div)
         self.loadLocalPlayer(false)
 
-        let wrap1 = document.createElement('div')
-        wrap1.className = 'yt-wrap'
-        self.step.appendChild(wrap1)
+        let wrap1 = $('<div class="yt-wrap" />')
+        self.step.append(wrap1)
 
-        let width = localStorage.getItem('ytWidth')
-        let left = localStorage.getItem('ytLeft')
         let top = localStorage.getItem('ytTop')
+        let left = localStorage.getItem('ytLeft')
+        let width = localStorage.getItem('ytWidth')
 
-        if (width !== null){ wrap1.style.width = width }
-        if (left !== null){ wrap1.style.left = left }
-        if (top !== null){ wrap1.style.top = top }
+        if (top !== null){ wrap1.css('top', top) }
+        if (left !== null){ wrap1.css('left', left) }
+        if (width !== null){ wrap1.css('width', width) }
 
-        let wrap2 = document.createElement('div')
-        wrap2.className = 'yt-wrapper'
-        wrap1.appendChild(wrap2)
+        let wrap2 = $('<div class="yt-wrapper" />')
+        wrap1.append(wrap2)
 
-        div = document.createElement('div')
-        div.setAttribute('id', 'yt-player')
-        wrap2.appendChild(div)
+        div = $('<div id="yt-player" />')
+        wrap2.append(div)
 
-        self.YTPlayer = new YT.Player(div, {
+        self.YTPlayer = new YT.Player(div[0], {
             videoId: self.youtubeId(),
             width: '640',
             height: '390',
@@ -242,11 +223,11 @@ const YTRS = {
                 listeners: {
                     start()
                     {
-                        wrap1.classList.add('dragging')
+                        wrap1.addClass('dragging')
                     },
                     end()
                     {
-                        wrap1.classList.remove('dragging')
+                        wrap1.removeClass('dragging')
                     },
                     move(event)
                     {
@@ -265,11 +246,11 @@ const YTRS = {
                 listeners: {
                     start()
                     {
-                        wrap1.classList.add('dragging')
+                        wrap1.addClass('dragging')
                     },
                     end()
                     {
-                        wrap1.classList.remove('dragging')
+                        wrap1.removeClass('dragging')
                     },
                     move(event)
                     {
@@ -355,16 +336,25 @@ const YTRS = {
     loadLocalPlayer: function(controls=false)
     {
         let self = this
-        let parent = document.getElementById('local-player')
+        let parent = $('#local-player')
+        let video = $('<video preload="metadata" />')
 
-        self.localVideoPlayer = document.createElement('video')
-        self.localVideoPlayer.setAttribute('preload', 'metadata')
         if (controls === true)
         {
-            self.localVideoPlayer.setAttribute('controls', 'true')
+            video.attr('controls', 'true')
         }
-        self.localVideoPlayer.onloadedmetadata = function(){ self.localVideoDuration = this.duration }
-        self.localVideoPlayer.oncanplay = function()
+
+        video.on('loadedmetadata', function()
+        {
+            self.localVideoDuration = this.duration
+        })
+
+        video.on('canplay', function()
+        {
+            self.localVideoValid = true
+        })
+
+        /*self.localVideoPlayer.oncanplay = function()
         {
             self.localVideoValid = true
 
@@ -391,23 +381,24 @@ const YTRS = {
 
                 self.currentVolume = self.localVideoPlayer.volume
             }
-        }
-        self.localVideoPlayer.onerror = function()
-        {
-            self.localVideoValid = false
-            alert('Invalid video format, please choose another file.')
-        }
+        }*/
 
-        let source = document.createElement('source')
-        source.setAttribute('src', self.localVideoURL)
-        source.addEventListener('error', function()
+        video.on('error', function()
         {
             self.localVideoValid = false
             alert('Invalid video format, please choose another file.')
         })
 
-        self.localVideoPlayer.appendChild(source)
-        parent.appendChild(self.localVideoPlayer)
+        let source = $(`<source src="${self.localVideoURL}" />`)
+        source.on('error', function()
+        {
+            self.localVideoValid = false
+            alert('Invalid video format, please choose another file.')
+        })
+
+        video.append(source)
+        parent.append(video)
+        self.localVideoPlayer = video[0]
     },
     youtubeId: function() {
         let id = ''
@@ -423,7 +414,7 @@ const YTRS = {
     },
     _clear: function()
     {
-        this.step.innerHTML = ''
+        this.step.html('')
     },
 }
 
